838f8e1e89642ef95e37ac6843ae3872ec1048b9
OlehKurachenko <oleh.kurachenko@gmail.com>
2018-06-06 18:07:34 +0300

    dependecies and Makefile written, project structure created

---
+++ src/exports/includes/filler.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okurache <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/06 17:48:13 by okurache          #+#    #+#             */
+/*   Updated: 2018/06/06 17:48:13 by okurache         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLER_FILLER_H
+# define FILLER_FILLER_H
+
+# include "../../../imports/okurache_libft/libft.h"
+# include "../../../imports/okurache_libftin/libftin.h"
+# include "../../../imports/okurache_ft_printf/ft_printf.h"
+
+
+
+#endif
